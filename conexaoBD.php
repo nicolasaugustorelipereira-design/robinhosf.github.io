b59eedb84e3ba29c87cc23a01d301ab5e2158c6c
robinhosf <nicolasaugustorelipereira@gmail.com>
2025-10-16 20:29:48 -0300

    Update database connection details in conexaoBD.php upar
    mudei para upar em um local para rodar o php o banco de dados deve retirar a parte do create table if not exist

--- conexaoBD.php
+++ conexaoBD.php
@@ -1,8 +1,13 @@
 <?php
-$servername = "localhost"; 
-$username = "root"; 
-$password = '';   
-$database = 'repositoriods';
+//$servername = "localhost"; 
+//$username = "root"; 
+//$password = '';   
+//$database = 'repositoriods';
+
+$servername = "robinhosf.gt.tc";       // host do banco 
+$username = "if0_40187066";             // usuário MySQL
+$password = "xpTEW1qM4tL4hc";           //  senha
+$database = "repositoriods";             // nome do banco (sem o .sql pois é extensão)
 
 $conn = new mysqli($servername, $username, $password, $database);
 
@@ -10,3 +15,4 @@ if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
 ?>
+
